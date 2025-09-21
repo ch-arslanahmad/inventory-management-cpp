685bc60d320f22a73c0c9ff76642b73b7b8892c0
ch-arslanahmad <ch.arslanad@gmail.com>
2025-09-21 14:44:08 +0500

    feat(Product.h): added a constructor and added getters

--- Product.h
+++ Product.h
@@ -15,6 +15,14 @@ public:
         this->quantity = quantity;
     }
 
+    // Constructor 2
+    Product(std::string name)
+    {
+        this->name = name;
+    }
+
+    // GETTERS
+
     std::string getName()
     {
         return name;
@@ -26,5 +34,29 @@ public:
     int getQuantity()
     {
         return quantity;
+    }
+
+    // SETTERS
+
+    void setItem(std::string name, double price, int quantity)
+    {
+        this->name = name;
+        this->price = price;
+        this->quantity = quantity;
+    }
+
+    void setName(std::string name)
+    {
+        this->name = name;
+    }
+
+    void setPrice(double price)
+    {
+        this->price = price;
+    }
+
+    void setName(int quantity)
+    {
+        this->quantity = quantity;
     }
 };
